3abbca1bfda0c18ae8d8f2cad004a9af39ab6f43
Gabriel <gabrielgarcindo14@gmail.com>
2026-05-25 14:03:39 -0300

    feat(fix): ajuste no processamento da porta do servidor

--- api2/construindoAPI-7.js
+++ api2/construindoAPI-7.js
@@ -125,7 +125,7 @@ const server = http.createServer((req, res) => {
     }));
 
 });
-const PORT = process.end.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running in ${PORT}`);
 });
